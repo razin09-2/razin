8e7bea7de1b54f1a305291926ebbd844c1165cdf
Razin <139611821+razin09-2@users.noreply.github.com>
2024-08-20 22:24:24 +0530

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
 namespace SpriteKind {
     export const pop1 = SpriteKind.create()
+    export const pic = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    sprites.destroy(mySprite2)
     if (dot.x == 2 && dot.y == 0) {
         tiles.setTileAt(tiles.getTileLocation(1, 9), assets.tile`myTile1`)
         tiles.setCurrentTilemap(tilemap`level4`)
     }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level2`)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     mySprite2 = sprites.create(img`
@@ -56,7 +61,32 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, l
     mySprite.setPosition(125, 30)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
-    game.showLongText(game.ask(game.askForString("oa", 2)), DialogLayout.Full)
+    if (game.askForString("what is the number and number minus one?", 2) == "ed") {
+        mySprite.setPosition(125, 30)
+        mySprite4 = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . f f f f . . . . . . . . 
+            . . . f d d d d f e e . . . . . 
+            . . . . f f f d d e e . . . . . 
+            . . . . . . . e 1 d f . . . . . 
+            . . . . . . e e e d d f . . . . 
+            . . . . . e e e . f d f . . . . 
+            . . . . e e e . . f d f . . . . 
+            . . . e e e . . . f d f . . . . 
+            . . e e e . . . . . f . . . . . 
+            . . e e . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.pic)
+        mySprite4.setFlag(SpriteFlag.RelativeToCamera, true)
+        scaling.scaleToPixels(mySprite4, 32, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    } else {
+        mySprite.setPosition(125, 30)
+        game.setGameOverMessage(false, "GAME OVER!")
+    }
 })
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     Render.moveWithController(2, 2, 2)
@@ -81,8 +111,9 @@ controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite3.setFlag(SpriteFlag.RelativeToCamera, true)
 })
 let mySprite3: Sprite = null
-let mySprite2: Sprite = null
+let mySprite4: Sprite = null
 let dot: Sprite = null
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
 mySprite = Render.getRenderSpriteVariable()
 scene.setBackgroundImage(img`
